88d5af2efed56711ff35ecaae89f26c551ff1709
Eduardo Braun <eduardotbraun@gmail.com>
2021-02-02 11:18:12 -0300

    dynamic functions

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,15 @@
-use wasmer::{Instance, Module, Store, Function, imports};
+use wasmer::{Instance, Module, Store, Function, FunctionType, Type, Value, imports};
 use wasmer_compiler_cranelift::Cranelift;
 use wasmer_engine_jit::JIT;
 use rand::thread_rng;
 use rand::Rng;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 
 fn rand32(a: u32, b: u32) -> u32 {
     let mut rng = thread_rng();
     rng.gen_range(a..=b)
 }
-
-fn guess(n: u32) -> i8 {
-    println!("Guessed: {}", n);
-    if n < 42 {
-        return -1;
-    }
-
-    if n > 42 {
-        return 1;
-    }
-
-    std::process::exit(0);
-}
-
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let wasm_path = concat!(
@@ -39,7 +27,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let module = Module::new(&store, wasm_bytes)?;
 
     let rand32 = Function::new_native(&store, rand32);
-    let guess = Function::new_native(&store, guess);
+
+    // Shared stop condition
+    let guessed = Arc::new(AtomicBool::new(false));
+    let guessed_closure = guessed.clone();
+
+    // Dynamic function capturing our atomic
+    let guess_sig = FunctionType::new(vec![Type::I32], vec![Type::I32]);
+    let guess = Function::new(&store, &guess_sig, move |args| {
+        let n = args[0].unwrap_i32() as u32;
+        println!("Guessed: {}", n);
+        if n < 42 {
+            return Ok(vec![Value::I32(-1)]);
+        }
+
+        if n > 42 {
+            return Ok(vec![Value::I32(1)]);
+        }
+
+        guessed_closure.store(true, Ordering::SeqCst);
+
+        Ok(vec![Value::I32(0)])
+    });
+
     // Create an import object.
     let import_object = imports! {
         "env" => {
@@ -54,8 +64,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Gets a refence to the `guess` function
     let turn = instance.exports.get_function("turn")?;
 
-    // Calls and print the result of our wasm function
-    loop {
+    // Calls the wasm function while not guessed
+    while !guessed.load(Ordering::SeqCst) {
         turn.call(&[])?;
     }
+    Ok(())
 }
